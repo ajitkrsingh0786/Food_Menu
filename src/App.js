09d59dcff7a7cffcf71833a8e1ddfd667787e5ef
ajitkrsingh0786 <ajitkrsingh0786@gmail.com>
2020-06-12 12:46:21 +0530

    Display

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React,{Component} from 'react';
 //import logo from './logo.svg';
 import {Navbar,NavbarBrand} from 'reactstrap';
 import './App.css';
+import {DISHES} from './shared/dishes';
 //import { render } from '@testing-library/react';
 import Menu from './Components/MenuComponent';
 class App extends Component {
+  constructor(props){
+    super(props);
+      this.state ={
+           dishes:DISHES
+      }
+  }
+  
+
 render(){
   return (
     <div className="App">
@@ -15,7 +24,7 @@ render(){
         
         </div>
         </Navbar>
-            <Menu/>
+            <Menu dishes={this.state.dishes}/>
     </div>
   );
 }
